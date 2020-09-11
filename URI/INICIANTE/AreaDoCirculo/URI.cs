a3927fef578c52cbcdaf0258fd008f23528b2935
lucasbenhur <lucas.ben.hur@hotmail.com>
2020-09-11 12:46:47 -0300

    Extremamente Básico Área do Círculo

---
+++ URI/INICIANTE/AreaDoCirculo/URI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double n, raio, area;
+
+        raio = double.Parse(Console.ReadLine());
+
+        n = 3.14159;
+
+        area = (raio*raio)*n;
+
+        Console.WriteLine("A=" + area.ToString("0.0000"));
+    }
+
+}
---
+++ URI/INICIANTE/ExtremamenteBasico/URI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        var strA = Console.ReadLine();
+        var strB = Console.ReadLine();
+
+        int A;
+        int B;
+
+        Int32.TryParse(strA, out A);
+        Int32.TryParse(strB, out B);
+
+        var X = A + B;
+
+        Console.WriteLine("X = " + X);
+    }
+
+}
