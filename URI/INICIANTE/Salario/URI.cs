f948d0386e8312fa32d01eca1bc98c62d3346ba4
lucasbenhur <lucas.ben.hur@hotmail.com>
2020-09-11 19:41:03 -0300

    Salario

---
+++ URI/INICIANTE/Salario/URI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int numero, qtdHoras;
+        float valorHora, salario;
+
+        numero = int.Parse(Console.ReadLine());
+        qtdHoras = int.Parse(Console.ReadLine());
+        valorHora = float.Parse(Console.ReadLine());
+
+        salario = qtdHoras * valorHora;
+
+        Console.WriteLine("NUMBER = " + numero);
+        Console.WriteLine("SALARY = U$ " + salario.ToString("F2"));
+    }
+}
