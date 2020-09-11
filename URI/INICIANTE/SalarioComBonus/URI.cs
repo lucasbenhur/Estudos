abda8d461724aa53b6d42dcae35b90a76cac8906
lucasbenhur <lucas.ben.hur@hotmail.com>
2020-09-11 19:55:08 -0300

    Salário com Bonus.

---
+++ URI/INICIANTE/SalarioComBonus/URI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var nome = Console.ReadLine();
+
+        double salarioFixo, totalVendas, totalReceber;
+
+        salarioFixo = double.Parse(Console.ReadLine());
+        totalVendas = double.Parse(Console.ReadLine());
+
+        totalReceber = salarioFixo + ((totalVendas * 15) / 100);
+
+        Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2"));
+    }
+}
