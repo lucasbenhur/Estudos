ead2d2d552ec831435e870df7dcea7f2fc36326c
lucasbenhur <lucas.ben.hur@hotmail.com>
2020-09-11 16:59:26 -0300

    Média 1

---
+++ URI/INICIANTE/Media1/URI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double a, b, media;
+
+        a = double.Parse(Console.ReadLine());
+        b = double.Parse(Console.ReadLine());
+
+        media = ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5);
+
+        Console.WriteLine("MEDIA = " + media.ToString("F5"));
+    }
+}
