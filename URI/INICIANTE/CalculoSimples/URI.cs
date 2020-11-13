b092f5b07d9e96117b81c36a16fd03a41c7ed9bf
lucasbenhur <lucas.ben.hur@hotmail.com>
2020-11-13 06:47:28 -0300

    Calculo Simples Esfera

---
+++ URI/INICIANTE/CalculoSimples/URI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var peca1 = Console.ReadLine();
+        var peca2 = Console.ReadLine();
+
+        int qtdePeca1 = int.Parse(peca1.Split(' ')[1]);
+        double valorPeca1 = double.Parse(peca1.Split(' ')[2]);
+
+        int qtdePeca2 = int.Parse(peca2.Split(' ')[1]);
+        double valorPeca2 = double.Parse(peca2.Split(' ')[2]);
+
+        var calculo = (valorPeca1 * qtdePeca1) + (valorPeca2 * qtdePeca2);
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + calculo.ToString("F2"));
+    }
+}
---
+++ URI/INICIANTE/Esfera/URI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        const double pi = 3.14159;
+
+        var raio = double.Parse(Console.ReadLine());
+
+        var volume = (4.0 / 3.0) * pi * Math.Pow(raio, 3.0);
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+    }
+}
