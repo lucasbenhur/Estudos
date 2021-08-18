3fa38495b220d11f5220e8c10fab18cee880bbd7
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-08-18 00:37:52 -0300

    - Reestruturação da solution. - 1013 - O Maior - 1014 - Consumo

---
+++ URI/1013 - O Maior/URI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var entrada = Console.ReadLine();
+
+        var A = int.Parse(entrada.Split(' ')[0]);
+        var B = int.Parse(entrada.Split(' ')[1]);
+        var C = int.Parse(entrada.Split(' ')[2]);
+
+        var maiorAB = (A + B + Math.Abs(A - B)) / 2;
+
+        var maiorABC = (maiorAB + C + Math.Abs(maiorAB - C)) / 2;
+
+        Console.WriteLine($"{maiorABC} eh o maior");
+    }
+}
---
+++ URI/1014 - Consumo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var km = int.Parse(Console.ReadLine());
+        var l = double.Parse(Console.ReadLine());
+
+        var resultado = km / l;
+
+        Console.WriteLine($"{resultado:F3} km/l");
+    }
+}
