1246d73729079df3432002aa0a3277d0a3880d26
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-08-21 08:47:07 -0300

    1019, 1020

---
+++ URI/1019 - Conversao de Tempo/URI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int valor;
+
+        valor = int.Parse(Console.ReadLine());
+
+        int hora = valor / 3600;
+        int minuto = (valor - (hora * 3600)) / 60;
+        int segundo = (valor - (hora * 3600) - (minuto * 60));
+
+        Console.WriteLine($"{hora}:{minuto}:{segundo}");
+    }
+}
---
+++ URI/1020 - Idade em Dias/URI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int valor;
+
+        valor = int.Parse(Console.ReadLine());
+
+        int anos = valor / 365;
+        int meses = (valor - (anos * 365)) / 30;
+        int dias = valor - (anos * 365) - (meses * 30);
+
+        Console.WriteLine($"{anos} ano(s)");
+        Console.WriteLine($"{meses} mes(es)");
+        Console.WriteLine($"{dias} dia(s)");
+    }
+}
