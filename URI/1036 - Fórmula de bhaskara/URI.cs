37f6435290de6d30e5853d2a90df6f1e85884d00
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-08-25 20:16:16 -0300

    1036

---
+++ URI/1036 - Fórmula de bhaskara/URI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        string entrada;
+        double A, B, C, delta, x1, x2;
+
+        entrada = Console.ReadLine();
+
+        A = double.Parse(entrada.Split(' ')[0]);
+        B = double.Parse(entrada.Split(' ')[1]);
+        C = double.Parse(entrada.Split(' ')[2]);
+
+        delta = Math.Pow(B, 2) - 4 * A * C;
+
+        if (delta >= 0 && 2 * A != 0)
+        {
+            x1 = (-B + Math.Sqrt(delta)) / (2 * A);
+            x2 = (-B - Math.Sqrt(delta)) / (2 * A);
+
+            Console.WriteLine($"R1 = {x1:F5}");
+            Console.WriteLine($"R2 = {x2:F5}");
+        }
+        else
+        {
+            Console.WriteLine("Impossivel calcular");
+        }
+    }
+}
