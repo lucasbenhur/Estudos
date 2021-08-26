d596f5e5526362a1c66678e8b9a11cf1aa2b3ffc
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-08-25 22:33:43 -0300

    1037

---
+++ URI/1037 - Intervalo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double valor;
+
+        valor = double.Parse(Console.ReadLine());
+
+        if (valor < 0 || valor > 100)
+        {
+            Console.WriteLine("Fora de intervalo");
+        }
+        else
+        {
+            if (valor >= 0 && valor <= 25)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else
+            {
+                if (valor > 25 && valor <= 50)
+                {
+                    Console.WriteLine("Intervalo (25,50]");
+                }
+                else
+                {
+                    if (valor > 50 && valor <= 75)
+                    {
+                        Console.WriteLine("Intervalo (50,75]");
+                    }
+                    else
+                    {
+                        if (valor > 75 && valor <= 100)
+                        {
+                            Console.WriteLine("Intervalo (75,100]");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
