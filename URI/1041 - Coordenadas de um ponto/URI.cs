3d83ee4e6bc28b576a53303644cf4dec35092a06
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-09-03 08:11:24 -0300

    1041, 1042

---
+++ URI/1041 - Coordenadas de um ponto/URI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        string entrada = Console.ReadLine();
+
+        double x = double.Parse(entrada.Split(' ')[0]);
+        double y = double.Parse(entrada.Split(' ')[1]);
+
+        if (x == 0 && y == 0)
+            Console.WriteLine("Origem");
+
+        if (x == 0 && y != 0)
+            Console.WriteLine("Eixo Y");
+
+        if (x != 0 && y == 0)
+            Console.WriteLine("Eixo X");
+
+        if (x > 0 && y > 0)
+            Console.WriteLine("Q1");
+
+        if (x < 0 && y > 0)
+            Console.WriteLine("Q2");
+
+        if (x < 0 && y < 0)
+            Console.WriteLine("Q3");
+
+        if (x > 0 && y < 0)
+            Console.WriteLine("Q4");
+    }
+}
---
+++ URI/1042 - Sort Simples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string entrada = Console.ReadLine();
+
+        var listaComoFoiLido = new List<int>();
+
+        listaComoFoiLido.Add(int.Parse(entrada.Split(' ')[0]));
+        listaComoFoiLido.Add(int.Parse(entrada.Split(' ')[1]));
+        listaComoFoiLido.Add(int.Parse(entrada.Split(' ')[2]));
+
+        var listaOrdemCrescente = listaComoFoiLido.OrderBy(x => x);
+
+        foreach (var item in listaOrdemCrescente)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine();
+
+        foreach (var item in listaComoFoiLido)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
