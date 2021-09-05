e7ad382b0b6ae028cea5731b80a94c5c09c2694e
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-09-05 20:13:16 -0300

    1043, 1044

---
+++ URI/1043 - Triangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b, c;
+        string entrada = Console.ReadLine();
+
+        a = double.Parse(entrada.Split(' ')[0]);
+        b = double.Parse(entrada.Split(' ')[1]);
+        c = double.Parse(entrada.Split(' ')[2]);
+
+        if (b - c < a &&
+            b + c > a &&
+            a - c < b &&
+            a + c > b &&
+            a - b < c &&
+            a + b > c)
+        {
+            double perimetro = a + b + c;
+
+            Console.WriteLine($"Perimetro = {perimetro:F1}");
+        }
+        else
+        {
+            double area = ((a + b) * c) / 2;
+
+            Console.WriteLine($"Area = {area:F1}");
+        }
+    }
+}
---
+++ URI/1044 - Multiplos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool saoMultiplos = false;
+        int a, b;
+        string entrada = Console.ReadLine();
+
+        a = int.Parse(entrada.Split(' ')[0]);
+        b = int.Parse(entrada.Split(' ')[1]);
+
+        if (a > b &&
+            a % b == 0)
+            saoMultiplos = true;
+
+        if (b > a &&
+            b % a == 0)
+            saoMultiplos = true;
+
+        Console.WriteLine(saoMultiplos ? "Sao Multiplos" : "Nao sao Multiplos");
+    }
+}
