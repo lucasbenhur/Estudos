09e94fc56ff95f160a8eb01d0b2d6b9424795d26
lucas.alcantara <lucas.alcantara@hubconexa.com>
2022-06-14 07:56:29 -0300

    1049

---
+++ URI/1049 - Animal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+
+        var nome = "Lucas";
+
+        Console.WriteLine("Bem vindo " + nome);
+    }
+}
